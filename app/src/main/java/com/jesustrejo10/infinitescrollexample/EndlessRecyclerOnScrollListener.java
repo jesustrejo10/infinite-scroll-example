b996091e3ee9fe9748688924667cefe1bdf08907
Jesus Trejo <jesustrejo10@gmail.com>
2019-04-21 09:18:35 -0400

    Implementing infinite scroll in detail fragment.

---
+++ app/src/main/java/com/jesustrejo10/infinitescrollexample/EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,55 @@
+package com.jesustrejo10.infinitescrollexample;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * @author Jesus Trejo on 4/21/19.
+ */
+
+public abstract class EndlessRecyclerOnScrollListener extends RecyclerView.OnScrollListener {
+    public static String TAG = EndlessRecyclerOnScrollListener.class.getSimpleName();
+
+    private boolean loading = false; // True if we are still waiting for the last set of data to load.
+
+    private int current_page = 1;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    public EndlessRecyclerOnScrollListener(LinearLayoutManager linearLayoutManager) {
+        this.mLinearLayoutManager = linearLayoutManager;
+    }
+
+    @Override
+    public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+
+        if(dy < 0) {
+            return;
+        }
+        // check for scroll down only
+        int visibleItemCount = recyclerView.getChildCount();
+        int totalItemCount = mLinearLayoutManager.getItemCount();
+        int firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        // to make sure only one onLoadMore is triggered
+        synchronized (this) {
+            int visibleThreshold = 2;
+            if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+                // End has been reached, Do something
+                current_page++;
+                onLoadMore(current_page);
+                loading = true;
+            }
+        }
+    }
+
+
+    public void setLoading(boolean loading) {
+        this.loading = loading;
+    }
+
+    public abstract void onLoadMore(int current_page);
+
+}
---
+++ app/src/main/java/com/jesustrejo10/infinitescrollexample/personList/PersonListFragment.kt
@@ -0,0 +1,166 @@
+package com.jesustrejo10.infinitescrollexample.personList
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jesustrejo10.infinitescrollexample.EndlessRecyclerOnScrollListener
+import com.jesustrejo10.infinitescrollexample.R
+import kotlinx.android.synthetic.main.fragment_person_list.*
+import android.support.v4.widget.SwipeRefreshLayout
+
+
+
+
+/**
+ * @author Jesus Trejo on 4/19/19.
+ */
+class PersonListFragment : Fragment(){
+
+	private lateinit var mScrollListener : EndlessRecyclerOnScrollListener
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		val rootView = inflater.inflate(R.layout.fragment_person_list, container, false)
+		return rootView
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		manageViewComponents()
+	}
+
+	private fun manageViewComponents() {
+
+
+		val animalNames = ArrayList<String>()
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+		animalNames.add("Horse")
+		animalNames.add("Cow")
+		animalNames.add("Camel")
+		animalNames.add("Sheep")
+		animalNames.add("Goat")
+
+
+
+
+
+
+
+
+		val adapter = PersonListAdapter(animalNames)
+		board_topic_list.layoutManager = LinearLayoutManager(context)
+		board_topic_list.adapter = adapter
+
+
+		mScrollListener = object : EndlessRecyclerOnScrollListener(LinearLayoutManager(context)) {
+			override fun onLoadMore(current_page: Int) {
+				setLoading(true)
+				swipeRefreshLayout.isRefreshing=true
+				voidea()
+				println("hola")
+
+			}
+		}
+
+		board_topic_list.addOnScrollListener(mScrollListener);
+
+
+		swipeRefreshLayout.setOnRefreshListener(SwipeRefreshLayout.OnRefreshListener {
+			// do something
+
+			// after refresh is done, remember to call the following code
+
+			if (swipeRefreshLayout != null && swipeRefreshLayout.isRefreshing()) {
+				swipeRefreshLayout.setRefreshing(false)  // This hides the spinner
+			}
+		})
+
+
+
+
+	}
+
+	private fun voidea() {
+
+		Handler().postDelayed({
+			mScrollListener.setLoading(false)
+			swipeRefreshLayout.isRefreshing=false
+		}, 2000)
+
+
+		// after loading is done, please call the following method to re-enable onLoadMore
+		// usually it should be called in onCompleted() method
+	}
+}
