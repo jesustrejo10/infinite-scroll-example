c38d5f3e873ae21233838bcf048866dfbb0d0dd1
Jesus Trejo <jesustrejo10@gmail.com>
2019-04-21 09:40:40 -0400

    Starting to implement design, architecture.

---
+++ app/src/main/java/com/jesustrejo10/infinitescrollexample/ui/TabPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.jesustrejo10.infinitescrollexample.ui
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.jesustrejo10.infinitescrollexample.ui.personList.PersonListFragment
+
+/**
+ * @author Jesus Trejo on 4/19/19.
+ */
+class TabPagerAdapter(fm: FragmentManager, private var tabCount: Int) :
+	FragmentPagerAdapter(fm) {
+
+	override fun getItem(position: Int): Fragment? {
+
+		when (position) {
+			0 -> return PersonListFragment()
+			1 -> return TabActivity.PlaceholderFragment.newInstance(2)
+			2 -> return TabActivity.PlaceholderFragment.newInstance(3)
+			3 -> return TabActivity.PlaceholderFragment.newInstance(4)
+			else -> return null
+		}
+	}
+
+	override fun getCount(): Int {
+		return tabCount
+	}
+}
---
+++ app/src/main/java/com/jesustrejo10/infinitescrollexample/ui/personList/PeopleListPresenter.kt
@@ -0,0 +1,13 @@
+package com.jesustrejo10.infinitescrollexample.ui.personList
+
+/**
+ * @author Jesus Trejo on 4/21/19.
+ */
+class PeopleListPresenter(){
+
+
+
+
+
+
+}
